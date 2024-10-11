7d22c505fb65f334e8d71498ee1507bb20d5c8c4
itsrmdhnaz <ramadhan.abdul.aziz14@gmail.com>
2024-10-12 02:36:48 +0700

    Menggunakan welcome screen di dosen

--- app/src/main/java/org/d3if3026/mobpro2/MainActivity.kt
+++ app/src/main/java/org/d3if3026/mobpro2/MainActivity.kt
@@ -7,11 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3026.mobpro2.ui.theme.Mobpro2Theme
+import org.d3if3026.mobpro2s.ui.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +20,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             Mobpro2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   WelcomeScreen(
+                       appLogo = R.mipmap.ic_launcher,
+                       appName = R.string.app_name,
+                       modifier = Modifier.padding(innerPadding)
+                   )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DosenAppPreview() {
     Mobpro2Theme {
-        Greeting("Android")
+        WelcomeScreen(
+            appLogo = R.mipmap.ic_launcher,
+            appName = R.string.app_name,
+        )
     }
 }
